3835d78224335936952f505d25608f66f40df6c9
jason9075 <jason9075@gmail.com>
2018-12-01 14:11:05 +0800

    add fonts

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from tensorflow.python import pywrap_tensorflow
 
 
@@ -36,8 +36,8 @@ def main():
         num_dets_t = graph.get_tensor_by_name('num_detections:0')
         classes_t = graph.get_tensor_by_name('detection_classes:0')
 
-        locations, scores, num_dets, classes= sess.run([boxes_t, scores_t, num_dets_t, classes_t],
-                                                       feed_dict={input_image: np.expand_dims(image, 0)})
+        locations, scores, num_dets, classes = sess.run([boxes_t, scores_t, num_dets_t, classes_t],
+                                                        feed_dict={input_image: np.expand_dims(image, 0)})
         locations = locations[0]
 
         print(str(scores))
@@ -48,15 +48,16 @@ def main():
 
         draw = ImageDraw.Draw(result)
         h, w, _ = image.shape
+        font = ImageFont.truetype("fonts/Raleway-Regular.ttf", 24)
         for index, location in enumerate(locations):
             if classes[0][index] != 1:  # filter person
                 continue
             if scores[0][index] < 0.65:
                 continue
-            point_1 = (int(location[1]*w), int(location[0]*h))
-            point_2 = (int(location[3]*w), int(location[2]*h))
-            draw.rectangle((point_1, point_2), width=2)
-            draw.text(point_1, str(index), fill='red')
+            point_1 = (int(location[1] * w), int(location[0] * h))
+            point_2 = (int(location[3] * w), int(location[2] * h))
+            draw.rectangle((point_1, point_2), outline='green')
+            draw.text(point_1, str(index), font=font, fill='red')
 
         result.show()
 
